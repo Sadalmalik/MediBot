98b90aa8662e52041c4dff5de28089e8ef2f18a7
Gleb Kuzmin <kuzmin@owlcat.games>
2025-07-02 20:41:17 +0200

    Почти рабочий раннер сценариев

--- Scenarist/toml.py
+++ Scenarist/toml.py
@@ -21,4 +21,4 @@ def dump(obj: Mapping[str, Any], fp: TextIO, /, *, multiline_strings: bool = Fal
 
 def dumps(obj: Mapping[str, Any], /, *, multiline_strings: bool = False, indent: int = 4):
     import tomli_w
-    tomli_w.dumps(obj, multiline_strings=multiline_strings, indent=indent)
+    return tomli_w.dumps(obj, multiline_strings=multiline_strings, indent=indent)
